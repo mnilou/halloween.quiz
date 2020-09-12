f91bf4617cb023371238af55f5780c326653ec02
Nilou Mostofi <nilou@km-direct.com>
2020-09-11 23:35:22 -0700

    started code and html design

---
+++ quiz.js
@@ -0,0 +1,73 @@
+// ### Instructions
+
+// * Then create code that will ask the user questions, one by one. The user must answer by hitting OK (for true) or Cancel (for false).
+
+// * Check the user's answer against the correct answer, and provide them with an alert telling them if they are right or wrong.
+
+// * **BONUS:** Keep track of the user's score.
+
+// creat question array
+var questions = [ {
+    q: "Was the 2012 theme 'House of Horrors', True or False?",
+    a: "True"
+},
+{
+    q: "Was the 2012 theme 'House of Horrors', True or False?",
+    a: "True"
+},
+{
+    q: "Lunging Pumpkin Carver first came out in 2015, True or False.",
+    a: "False, it was 2016"
+},
+{
+    q: "Jumping Spider has come back every year since 2013, True or False.",
+    a: "True"
+},
+{
+    q: "Was the 2013 theme 'Carnival of Horrors', True or False?",
+    a: "False, it was the theme in 2010-2011. The theme for 2013 was Spirit Asylum." 
+},
+{
+    q: "Spirit Halloween used to have a Zombie Flamingo, True or False.",
+    a: "True, from 2013 - 2018."
+},
+{
+    q: "Cerberus was an animatronic since 2015, True or False.",
+    a: "True"
+},
+{
+    q: "Was the 2014 theme 'The Attic', True or False?",
+    a: "True"
+},
+{
+    q: "Twitching Banshee is one of mama's favorite animatronics, True or False.",
+    a: "False"
+},
+{
+    q: "Caspian has a crush on Abandoned Annie, True or False",
+    a: "True😂"
+},
+{
+    q: "Zephyr has a crush on the Hauntress, True or False",
+    a: "True🤣"
+}
+];
+
+// initialize score to 0
+var score = 0;
+
+// for each question in the question array
+for (var i = 0; i < questions.length; i++) {
+    if ()
+
+}
+// display the question
+// get the user answer
+// if user answer is correct
+// add 1 to score
+// alert "correct"
+// if user answer is incorrect
+// alert "incorrect"
+// end for each question loop
+
+// alert "You answered 3 out of 5 questions correctly."
